b0037603769c42058dd6dad3273013b440a04305
pybiles <109141592+pybiles@users.noreply.github.com>
2023-04-17 19:40:10 +0800

    将注册中心改为Nacos 改造consumer 以组的方式区分配置文件

--- consumer-service/src/main/java/com/yjx/controller/FeignController.java
+++ consumer-service/src/main/java/com/yjx/controller/FeignController.java
@@ -2,6 +2,7 @@ package com.yjx.controller;
 
 import com.yjx.User;
 import com.yjx.client.ProviderFeignClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,14 @@ import javax.annotation.Resource;
 @RestController
 @RefreshScope//可以动态获取配置文件的更新
 public class FeignController {
+    @Value("${abc.cde}")
+    private String num;
     @Resource
     private ProviderFeignClient providerFeignClient;
 
     @RequestMapping("/feign/hello/{name}")
     public String feignHello(@PathVariable("name") String name) {
+        System.out.println(num);
         return providerFeignClient.hello(name);
     }
 
